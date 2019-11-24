1add2c0e11438f5debfef4039be16f38204be176
Sahil Singh <sahil.dogstar@gmail.com>
2019-11-24 22:46:27 +0530

    Day 34 morning challenge completed.

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,4 @@
+
 // Challenge 1
 // Minimum Removals to Make Two Strings Anagrams
 // Create a function that returns the smallest number of letter removals so that
@@ -27,5 +28,91 @@
 // - false if they aren't anagrams, or
 // - Their Hamming distance if they are anagrams with >=1 letter at the same index.
 
-main() {
+int minRemovals(String firstWord, String secondWord) {
+  if (firstWord.isEmpty && secondWord.isEmpty) {
+    return 0;
+  }
+  else if (firstWord.isEmpty) {
+    return secondWord.length;
+  }
+  else if (secondWord.isEmpty) {
+    firstWord.length;
+  }
+  else {
+    List firstString = firstWord.split("").toList();
+    List secondString = secondWord.split("").toList();
+
+    int count = 0;
+    for (int i = 0; i < firstString.length; i++) {
+      if (secondString.contains(firstString[i])) {
+        int removeIndex = secondString.indexWhere((element) =>
+        element == (firstString[i]));
+        secondString.removeAt(removeIndex);
+      }
+      else {
+        count++;
+      }
+    }
+    return count + secondString.length;
+  }
 }
+
+
+
+
+dynamic farthestHamming(String firstWord, String secondWord){
+  if(checkForAnagram(firstWord, secondWord) && hammingDistance(firstWord, secondWord) == firstWord.length){
+    return true;
+  }
+  else if(!checkForAnagram(firstWord, secondWord)){
+    return false;
+  }
+  else if(checkForAnagram(firstWord, secondWord) && hammingDistance(firstWord, secondWord) >= 1){
+    return hammingDistance(firstWord, secondWord);
+  }
+}
+
+
+bool checkForAnagram(String wordOne, String wordTwo){
+
+  List <String> firstWord = wordOne.split("").toList();
+  List <String> secondWord =  wordTwo.split("").toList();
+
+  if(wordOne.length != wordTwo.length){
+    return false;
+  }
+  else {
+    for(int i = 0; i<firstWord.length; i++){
+      if(secondWord.contains(firstWord[i])){
+        secondWord.remove(firstWord[i]);
+      }
+    }
+    if(secondWord.isEmpty){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
+
+int hammingDistance(String first, String second){
+  List firstWord = first.split("").toList();
+  List secondWord = second.split("").toList();
+  int count = 0;
+  for(int i = 0; i< firstWord.length; i++){
+    if(firstWord[i]!=secondWord[i]){
+      count++;
+    }
+  }
+  return count;
+}
+
+
+main() {
+  print(minRemovals("hello", "cello"));
+}
+
+
+
+
--- test/Day_34_Morning_Challenge_test.dart
+++ test/Day_34_Morning_Challenge_test.dart
@@ -1,4 +1,19 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
+  test("Test case for minRemovals", (){
+    expect(minRemovals("abcde", "cab"), 2);
+    expect(minRemovals("met", "meet"), 1);
+    expect(minRemovals("hello", "cello"), 2);
+    expect(minRemovals("mississippi", "pope"), 11);
+  });
+
+  test("Test case for anagram", (){
+    expect(checkForAnagram("balloon", "balooon"), false);
+  });
 }
+
+
+
+
